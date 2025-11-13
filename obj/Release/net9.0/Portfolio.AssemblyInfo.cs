bf83a63eda9aeaecbc016a5a3b219b478346f3c6
Minecart <andrew.krause08@gmail.com>
2025-11-12 23:01:34 -0600

    BRUH I MISSED A SINGLE FORWARD SLASH OH MY GOSHHHHHHHHHHHHHHHHHHHH

--- obj/Release/net9.0/Portfolio.AssemblyInfo.cs
+++ obj/Release/net9.0/Portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05aef5c8f79beb00466b088282e1e594f20306a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02633b791e0e7680355f50f96a588348b5cf4b68")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
